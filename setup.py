f84d6132ec139c29e75ac08585dfec6619292089
beisserm <beisserm@33789b7b-647c-422d-f633-bdd1f0b5e9eb>
2009-10-17 08:49:15 +0000

    Initial ci of the MDI sample wxPython widgets.
    git-svn-id: https://svn.kenai.com/svn/tilem~tilempy@1 33789b7b-647c-422d-f633-bdd1f0b5e9eb

---
+++ setup.py
@@ -0,0 +1,36 @@
+# A very simple setup script to create 2 executables.
+#
+# test_wx.py is a simple wxPython program, it will be converted into a
+# console-less program.
+#
+#
+# Run the build process by entering 'setup.py py2exe' or
+# 'python setup.py py2exe' in a console prompt.
+#
+# If everything works well, you should find a subdirectory named 'dist'
+# containing some files, among them hello.exe and test_wx.exe.
+#setup(
+#    # The first three parameters are not required, if at least a
+#    # 'version' is given, then a versioninfo resource is built from
+#    # them and added to the executables.
+#    version = "0.5.0",
+#    description = "py2exe sample script",
+#    name = "py2exe samples",
+#
+#    # targets to build
+#    windows = ["script": "tilem.py"],
+#    #console = ["hello.py"],
+#    )
+
+from distutils.core import setup
+import py2exe
+
+
+setup(
+    windows = [
+        {
+            "script": "tilem.py",
+            "icon_resources": [(1, "DeletedIcon.ico")]
+        }
+    ],
+)
---
+++ tilem.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+__author__="Matt Beisser"
+__date__ ="$Oct 16, 2009 6:33:40 PM$"
+
+import  wx
+
+# Importing ScrolledWindow demo to make use of the MyCanvas
+# class defined within.
+import  ScrolledWindow
+import  images
+
+SHOW_BACKGROUND = 1
+
+#----------------------------------------------------------------------
+ID_New  = wx.NewId()
+ID_Exit = wx.NewId()
+#----------------------------------------------------------------------
+
+class TilemFrame(wx.MDIParentFrame):
+	def __init__(self):
+
+		wx.MDIParentFrame.__init__(self, None, -1, "MDI Parent", size=(600,400))
+
+		self.winCount = 0
+		
+		self.CreateMenus()
+
+		
+		#filemenu = wx.Menu()
+		#editMenu = wx.Menu()
+		#menu = wx.Menu()
+		#menu.Append(ID_New, "&New Window")
+		#menu.AppendSeparator()
+		#menu.Append(ID_Exit, "E&xit")
+		#
+		#menubar = wx.MenuBar()
+		#menubar.Append(menu, "&File")
+		#self.SetMenuBar(menubar)
+		#self.CreateStatusBar()
+
+		self.Bind(wx.EVT_MENU, self.OnNewWindow, id=ID_New)
+		self.Bind(wx.EVT_MENU, self.OnExit, id=ID_Exit)
+
+		if SHOW_BACKGROUND:
+			self.bg_bmp = images.GridBG.GetBitmap()
+			self.GetClientWindow().Bind(
+				wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground
+			)
+		
+
+	def CreateMenus(self):
+		#filemenu = wx.Menu()
+		editMenu = wx.Menu()
+		menu = wx.Menu()
+		menu.Append(ID_New, "&New Window")
+		menu.AppendSeparator()
+		menu.Append(ID_Exit, "E&xit")
+		
+		menubar = wx.MenuBar()
+		menubar.Append(menu, "&File")
+		self.SetMenuBar(menubar)
+		self.CreateStatusBar()
+		self.winCount = self.winCount + 1		
+		
+		
+#####
+# Event handlers
+#####
+	def OnExit(self, evt):
+		self.Close(True)
+
+
+	def OnNewWindow(self, evt):
+		
+		self.winCount = self.winCount + 1
+		win = wx.MDIChildFrame(self, -1, "Child Window: %d" % self.winCount)
+		canvas = ScrolledWindow.MyCanvas(win)
+		win.Show(True)
+
+
+	def OnEraseBackground(self, evt):
+		dc = evt.GetDC()
+		evt.GetId()
+		if not dc:
+			dc = wx.ClientDC(self.GetClientWindow())
+
+		# tile the background bitmap
+		sz = self.GetClientSize()
+		w = self.bg_bmp.GetWidth()
+		h = self.bg_bmp.GetHeight()
+		x = 0
+
+		while x < sz.width:
+			y = 0
+
+			while y < sz.height:
+				dc.DrawBitmap(self.bg_bmp, x, y)
+				y = y + h
+
+			x = x + w
+
+
+#----------------------------------------------------------------------
+
+if __name__ == '__main__':
+	class MyApp(wx.App):
+		def OnInit(self):
+			wx.InitAllImageHandlers()
+			frame = TilemFrame()
+			frame.Show(True)
+			self.SetTopWindow(frame)
+			return True
+
+
+	app = MyApp(False)
+	app.MainLoop()
+
